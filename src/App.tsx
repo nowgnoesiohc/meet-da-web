c69372d6bbd8da1f3d1f5502d2f5c782eb49eb1b
nowgnoesiohc <csw6203@naver.com>
2024-12-26 16:39:21 +0900

    style : 메인 페이지 router, style 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import GlobalStyles from "./styles/GlobalStyle";
 
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f5f5f5;
+`;
+
 function App() {
   return (
-    <>
+    <AppContainer>
       <GlobalStyles />
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </AppContainer>
   );
 }
 
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "@/pages/Home/Home";
 import Page1 from "../pages/Page1/Page1";
 import Page2 from "../pages/Page2/Page2";
 
@@ -8,6 +9,10 @@ export const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
       {
         path: "/page1",
         element: <Page1 />,
