71563be03bb7ed87e4b35f1dec826fe05377c1a3
nowgnoesiohc <csw6203@naver.com>
2025-01-02 18:08:37 +0900

    fix : 공통 컴포넌트 사용예제 페이지 오류  수정

--- src/pages/Page2/Page2.tsx
+++ src/pages/Page2/Page2.tsx
@@ -1,6 +1,5 @@
 import GlobalStyle from "@/styles/GlobalStyle";
 import { Input, Textarea } from "@/components/ui/Input";
-import { Input1, Textarea1 } from "@/components/ui/Input1";
 
 export function Page1() {
   return (
@@ -18,14 +17,6 @@ export function Page1() {
 
           <h2>Textarea 예제</h2>
           <Textarea placeholder="내용을 입력하세요" maxLength={50} showCount />
-        </div>
-
-        <div style={{ width: "300px", margin: "50px auto" }}>
-          <h2>Input1 예제</h2>
-          <Input1 placeholder="이름을 입력하세요" maxLength={10} showCount />
-
-          <h2>Textarea1 예제</h2>
-          <Textarea1 placeholder="내용을 입력하세요" maxLength={50} showCount />
         </div>
       </div>
     </>
