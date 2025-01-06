81401f35521e79234f56fcc9586d921382690696
서준형 <luke3151>
2025-01-06 16:35:22 +0900

    style: 모달컴포넌트 퍼블리싱

---
+++ src/store/ModalStore.tsx
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+
+interface UseIsModalStoreType {
+  isModal: boolean | string;
+  setIsModalClick: (modalType?: string) => void;
+}
+
+export const useIsModalStore = create<UseIsModalStoreType>((set) => ({
+  isModal: false,
+  setIsModalClick: (modalType: string | boolean = false) => {
+    set(() => ({
+      isModal: modalType ? modalType : false,
+    }));
+  },
+}));
