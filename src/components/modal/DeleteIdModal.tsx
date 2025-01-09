c7777d38c8bb0af483ce942177dfa51557906310
noey <yoneee909@gmail.com>
2025-01-10 05:36:42 +0900

    modify : modal css 수정

--- src/components/modal/DeleteIdModal.tsx
+++ src/components/modal/DeleteIdModal.tsx
@@ -1,84 +1,84 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import { useIsModalStore } from "../../store/ModalStore";
 import { OrangeButton, OrangeLineButton } from "../ui/Button";
 
 const DeleteIdWrap = styled.div`
-    width:30.25rem;
-    background-color:#fff;
-    border-radius:1.875rem;
-    padding-top:3.125rem;
-    padding-bottom:2.875rem;
+  width: 30.25rem;
+  background-color: var(--white);
+  border-radius: 1.875rem;
+  padding-top: 3.125rem;
+  padding-bottom: 2.875rem;
 
-    @media (max-width:390px){
-        width:20rem;
-        margin:0 auto;
-        padding-top:2.375rem;
-        padding-bottom:1.75rem;
-    }
-`
+  @media (max-width: 390px) {
+    width: 20rem;
+    margin: 0 auto;
+    padding-top: 2.375rem;
+    padding-bottom: 1.75rem;
+  }
+`;
 
 const Title = styled.div`
-    > h2{
-        font-size:1.625rem;
-        font-weight:var(--font-semibold);
-        color:var(--main-orange);
-        margin:0;
-        padding-bottom:1.875rem;
-    }
-    
-    > p{
-        margin:0;
-        font-size:1.25rem;
-        font-weight:var(--font-medium);
-        color:var(--main-text);
-        padding-bottom:4.625rem;
-    }
+  > h2 {
+    font-size: 1.625rem;
+    font-weight: var(--font-semibold);
+    color: var(--main-orange);
+    margin: 0;
+    padding-bottom: 1.875rem;
+  }
 
-    @media (max-width:390px){
-        > h2{
-            font-size:1.5rem;
-        }
-        > p{
-            font-size:1.125rem;
-            padding-bottom:3.25rem;
-        }
+  > p {
+    margin: 0;
+    font-size: 1.25rem;
+    font-weight: var(--font-medium);
+    color: var(--main-text);
+    padding-bottom: 4.625rem;
+  }
+
+  @media (max-width: 390px) {
+    > h2 {
+      font-size: 1.5rem;
     }
-`
+    > p {
+      font-size: 1.125rem;
+      padding-bottom: 3.25rem;
+    }
+  }
+`;
 
 const Button = styled.div`
-    width:16.75rem;
-    height:3rem;
-    margin:0 auto;
-    display:flex;
-    justify-content:center;
-    gap:1.5rem;
+  width: 16.75rem;
+  height: 3rem;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  gap: 1.5rem;
 
-    > button{
-        width:7.625rem;
-        height:3rem;
-    }
-`
+  > button {
+    width: 7.625rem;
+    height: 3rem;
+  }
+`;
 
-export default function DeleteIdModal(){
+export default function DeleteIdModal() {
+  const setIsModalClick = useIsModalStore((state) => state.setIsModalClick);
+  const onClickCancel = () => {
+    setIsModalClick();
+  };
 
-    const setIsModalClick = useIsModalStore((state) => state.setIsModalClick); 
-    const onClickCancel = () => {
-        setIsModalClick();
-    }
-
-
-    return(
-        <>
-            <DeleteIdWrap>
-                <Title>
-                    <h2>회원 탈퇴</h2>
-                    <p>정말로 탈퇴 하시겠습니까?</p>
-                </Title>
-                <Button>
-                    <OrangeLineButton variant="modal" onClick={onClickCancel}>취소</OrangeLineButton>
-                    <OrangeButton variant="confirm">확인</OrangeButton>
-                </Button>
-            </DeleteIdWrap>
-        </>
-    )
+  return (
+    <>
+      <DeleteIdWrap>
+        <Title>
+          <h2>회원 탈퇴</h2>
+          <p>정말로 탈퇴 하시겠습니까?</p>
+        </Title>
+        <Button>
+          <OrangeLineButton variant="modal" onClick={onClickCancel}>
+            취소
+          </OrangeLineButton>
+          <OrangeButton variant="confirm">확인</OrangeButton>
+        </Button>
+      </DeleteIdWrap>
+    </>
+  );
 }
--- src/components/modal/FriendModal.tsx
+++ src/components/modal/FriendModal.tsx
@@ -1,246 +1,260 @@
-import styled from "styled-components"
-import CloseButton from "./CloseButton"
-import SearchImg from "../../assets/images/search.svg"
-import { FriendButton, FriendTabButton } from "../ui/Button"
-import { useState } from "react"
+import styled from "styled-components";
+import CloseButton from "./CloseButton";
+import { IoSearch } from "react-icons/io5";
+import { FriendButton, FriendTabButton } from "../ui/Button";
+import { useState } from "react";
 
 const Wrap = styled.div`
-    width:40.375rem;
-    background-color:var(--bg-01);
-    border-radius:1.25rem;
-    padding-top:5.375rem;
-    padding-bottom:2.875rem;
+  width: 40.375rem;
+  background-color: var(--bg-01);
+  border-radius: 1.25rem;
+  padding: 5.375rem 2.5rem 2.875rem;
 
-    @media (max-width:390px){
-        width:20rem;
-        margin:0 auto;
-        padding-top:4.125rem;
-    }
-`
+  @media (max-width: 390px) {
+    width: 20rem;
+    margin: 0 auto;
+    padding: 4.125rem 1.25rem 3.375rem;
+  }
+`;
 
-const SearchBar = styled.div`
-    width:34.75rem;
-    position:relative;
-    margin:0 auto;
+const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 35.375rem;
+  height: 3.75rem;
+  padding: 0.75rem 1.25rem 0.75rem 1.875rem;
+  border-radius: 0.625rem;
+  border: 0.0625rem solid var(--main-text);
+  background-color: var(--white);
+  margin: 0 auto;
 
-    > button{
-        position:absolute;
-        top:0.9375rem;
-        right:0.9375rem; 
-    }
+  @media (max-width: 390px) {
+    width: 100%;
+    padding: 12px 20px;
+  }
+`;
 
-    @media (max-width:390px){
-        width:17.5rem;
+const SearchInput = styled.input`
+  border: none;
+  background: transparent;
+  outline: none;
+  flex: 1;
+  font-size: 1.25rem;
+  font-weight: var(--font-regular);
 
-        > button{
-            top:1.25rem;
-        }
+  &::placeholder {
+    color: var(--sub-text);
+    font-weight: var(--font-regular);
+  }
 
-        > button > img{
-            width:1rem;
-            height:1rem;
-        }
-    }
-`
+  @media (max-width: 390px) {
+    width: 100%;
+    font-size: 1rem;
+  }
+`;
 
-const Input = styled.input`
-    width:100%;
-    height:3.75rem;
-    background-color:#fff;
-    border:0.0625rem solid var(--main-text);
-    border-radius:0.625rem;
-    padding-left:1.875rem;
-    font-size:1.25rem;
-    position:relative;
-    
-    &::placeholder {
-        color:var(--sub-text);
-    }
+const SearchButton = styled.button`
+  display: flex;
+`;
 
-    @media (max-width:390px){
-        font-size:0.875rem;
-        padding-left:1.25rem;
-    }
-`
+const SearchIcon = styled(IoSearch)`
+  color: var(--search-placeholder);
+  font-size: 1.5rem;
+`;
 
 const Profile = styled.div`
-    width:35.375rem;
-    height:3rem;
-    margin:1.875rem auto;
-    display:flex;
-    align-items:center;
-    justify-content:space-between;
+  width: 35.375rem;
+  height: 3rem;
+  margin: 1.875rem auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 20px;
 
-    > p{
-        font-size:1.5rem;
-        font-weight:var(--font-semibold);
+  > p {
+    font-size: 1.5rem;
+    font-weight: var(--font-semibold);
 
-        > span{
-            font-size:1.125rem;
-        }
+    > span {
+      font-size: 1.125rem;
     }
+  }
 
-    @media (max-width:390px){
-        > p{
-            margin:0;
-            padding-bottom:0.75rem;
+  @media (max-width: 390px) {
+    > p {
+      margin: 0;
+      padding-bottom: 0.75rem;
 
-             > span{
-                font-size:1rem;
-                font-weight:var(--font-regular);
-            }
-        }
+      > span {
+        font-size: 1rem;
+        font-weight: var(--font-regular);
+      }
     }
+  }
 
-    @media (max-width:390px){
-        width:17.5rem;
-        height:5.625rem;
-        display:block;
-        text-align:left;
-        margin:1.25rem auto;
-        padding:0 1rem;
-    }
-`
+  @media (max-width: 390px) {
+    width: 17.5rem;
+    height: 5.625rem;
+    display: block;
+    text-align: left;
+    margin: 1.25rem auto;
+    padding: 0 1rem;
+  }
+`;
 
 const Button = styled.div`
-    display:flex;
-    gap:1.25rem;
-`
+  display: flex;
+  gap: 1.25rem;
+`;
 
 const Content = styled.div`
-    width:35.375rem;
-    background-color:#fff;
-    margin:0 auto;
-    border-radius:0.75rem;
-    box-shadow:0.125rem 0.125rem 0.5rem rgba(0, 0, 0, 0.25);
-    padding:2.5rem;
-    
-    @media (max-width:390px){
-        width:17.5rem;
-        padding:1.75rem 1.25rem;
-    }
-`
+  width: 35.375rem;
+  background-color: var(--white);
+  margin: 0 auto;
+  border-radius: 0.75rem;
+  box-shadow: 0.125rem 0.125rem 0.5rem rgba(0, 0, 0, 0.25);
+  padding: 2.5rem;
+
+  @media (max-width: 390px) {
+    width: 17.5rem;
+    padding: 1.75rem 1.25rem;
+  }
+`;
 
 const FriendList = styled.div`
-    width:30.375rem;
-    height:3.25rem;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    gap:1rem;
+  width: 30.375rem;
+  height: 3.25rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
 
-    &:not(:last-child){
-        margin-bottom:1.75rem;
+  &:not(:last-child) {
+    margin-bottom: 1.75rem;
+  }
+
+  @media (max-width: 390px) {
+    width: 15rem;
+    height: 10.0625rem;
+    display: block;
+
+    > button {
+      float: right;
     }
-
-    @media (max-width:390px){
-        width:15rem;
-        height:10.0625rem;
-        display:block;
-
-        > button{
-            float:right;
-        }
-    }
-`
+  }
+`;
 
 const FriendProfile = styled.div`
-    width:23.125rem;
-    height:3.25rem;
-    border-radius:0.625rem;
-    background-color:var(--bg-02);
-    display:flex;
-    justify-content:space-around;
-    padding:0 1.25rem;
+  width: 23.125rem;
+  height: 3.25rem;
+  border-radius: 0.625rem;
+  background-color: var(--bg-02);
+  display: flex;
+  justify-content: space-around;
+  padding: 0 1.25rem;
 
-    @media (max-width:390px){
-        width:100%;
-        height:6.3125rem;
-        display:block;
-        margin-bottom:1rem;
-        padding-bottom:0.75rem;
-    }
-`
+  @media (max-width: 390px) {
+    width: 100%;
+    height: 6.3125rem;
+    display: block;
+    margin-bottom: 1rem;
+    padding-bottom: 0.75rem;
+  }
+`;
 
 const UserWrap = styled.div`
-    display:flex;
-    align-items:center;
-    gap:0.5rem;
-`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
 const UserImage = styled.div`
-    width:2.25rem;
-    height:2.25rem;
-    border-radius:6.25rem;
-    background-color:#000;
-`
+  width: 2.25rem;
+  height: 2.25rem;
+  border-radius: 6.25rem;
+  background-color: var(--black);
+`;
 
 const UserName = styled.p`
-    font-size:1.125rem;
-    font-weight:var(--font-semibold);
+  font-size: 1.125rem;
+  font-weight: var(--font-semibold);
 
-    @media (max-width:390px){
-        font-size:1.125rem;
-    }
-`
+  @media (max-width: 390px) {
+    font-size: 1.125rem;
+  }
+`;
 
 const UserText = styled.p`
-    font-size:1rem;
-    color:var(--line-basic);
-    
-    @media (max-width:390px){
-        text-align:left;
-        font-size:1rem;
-        margin:0;
-    }
-`
+  font-size: 1rem;
+  color: var(--line-basic);
 
-export default function FriendModal(){
-    const [isClicked, setIsClicked] = useState(true);
+  @media (max-width: 390px) {
+    text-align: left;
+    font-size: 1rem;
+    margin: 0;
+  }
+`;
 
-    const handleClick = () => {
-        setIsClicked(!isClicked); // 클릭 상태 토글
-    };
+export default function FriendModal() {
+  const [isClicked, setIsClicked] = useState(true);
 
-    return(
-        <>
-            <Wrap>
-                <CloseButton />
-                <SearchBar>
-                    <Input type="text" placeholder="믿다에서 새로운 친구를 만나보세요!" />
-                    <button><img src={SearchImg} alt="검색 이미지" /></button>
-                </SearchBar>
-                <Profile>
-                    <p>포뇨소스케스키<span>님의</span></p>
-                    <Button>
-                    <FriendTabButton
-                        isClicked={isClicked}
-                        onClick={handleClick}
-                        variant="unclicked"
-                    >
-                    서로 믿음
-                    </FriendTabButton>
-                    <FriendTabButton
-                        isClicked={isClicked}
-                        onClick={handleClick}
-                        variant="unclicked"
-                    >
-                    믿으미
-                    </FriendTabButton>
-                    </Button>
-                </Profile>
-                <Content>
-                    <FriendList>
-                        <FriendProfile>
-                            <UserWrap>
-                                <UserImage></UserImage>
-                                <UserName>믿음소망사과</UserName>
-                            </UserWrap>
-                            <UserText>믿음소망사랑 아니고 사과</UserText>
-                        </FriendProfile>
-                        <FriendButton variant="diaryFollow">만나기</FriendButton>
-                    </FriendList>
-                </Content>
-            </Wrap>
-        </>
-    )
+  const handleClick = () => {
+    setIsClicked(!isClicked); // 클릭 상태 토글
+  };
+
+  return (
+    <>
+      <Wrap>
+        <CloseButton />
+        {/* <SearchBar>
+          <Input type="text" placeholder="믿다에서 새로운 친구를 만나보세요!" />
+          <button>
+            <SearchIcon />
+          </button>
+        </SearchBar> */}
+        <SearchBarContainer>
+          <SearchInput
+            type="text"
+            placeholder="믿다에서 새로운 친구를 만나보세요!"
+          />
+          <SearchButton>
+            <SearchIcon />
+          </SearchButton>
+        </SearchBarContainer>
+        <Profile>
+          <p>
+            포뇨소스케스키<span>님의</span>
+          </p>
+          <Button>
+            <FriendTabButton
+              isClicked={isClicked}
+              onClick={handleClick}
+              variant="unclicked"
+            >
+              서로 믿음
+            </FriendTabButton>
+            <FriendTabButton
+              isClicked={isClicked}
+              onClick={handleClick}
+              variant="unclicked"
+            >
+              믿으미
+            </FriendTabButton>
+          </Button>
+        </Profile>
+        <Content>
+          <FriendList>
+            <FriendProfile>
+              <UserWrap>
+                <UserImage></UserImage>
+                <UserName>믿음소망사과</UserName>
+              </UserWrap>
+              <UserText>믿음소망사랑 아니고 사과</UserText>
+            </FriendProfile>
+            <FriendButton variant="diaryFollow">만나기</FriendButton>
+          </FriendList>
+        </Content>
+      </Wrap>
+    </>
+  );
 }
